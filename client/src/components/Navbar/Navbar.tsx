2a40627e0f71ba95db96fb2f83966f54c5d57dc7
Elie Paul <code@eliepaul.dev>
2025-01-22 07:55:28 -0500

    feat: :sparkles: created the navigation bar component

---
+++ client/src/components/Navbar/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+export default function Navbar() {
+    return (
+        <nav>
+            <h1>Seek.ai</h1>
+            <button name="donate">Donate</button>
+        </nav>
+    )
+}
