349d6c3e0efdd73e161bf9578f07eefc4e282eb1
Elie Paul <code@eliepaul.dev>
2025-01-28 23:33:06 -0500

    feat: :sparkles: added react router for navigation

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,20 @@
-import './App.css';
+import { useState } from 'react';
+import { Routes, Route } from 'react-router';
 import Layout from './layout/layout';
 import Home from './pages/Home/Home';
+import { useData } from './hooks/useData/useData';
+import './App.css';
+
 
 export default function App() {
+
 
   return (
     <div data-testid="app">
       <Layout>
-        <Home />
+        <Routes>
+          <Route index element={<Home />} />
+        </Routes>
       </Layout>
     </div>
   )
