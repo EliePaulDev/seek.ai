1a3f33a8845b8d2ab82e7174980b4fe595b404f0
Elie Paul <code@eliepaul.dev>
2025-02-13 15:14:57 -0500

    feat: :sparkles: connect backend with frontend and implement the search route

--- server/api/views.py
+++ server/api/views.py
@@ -1,7 +1,33 @@
-from django.shortcuts import render
-from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+import requests
+import environ
+
+env = environ.Env()
 
 # Create your views here.
+@api_view(["GET"])
 def search(request):
-    response = request.POST
-    return JsonResponse(response)
+    company_name = request.query_params["company"].strip('"')
+
+    headers = {
+        "X-Cb-user-key": env("DJANGO_CRUNCHBASE_API_KEY"),
+        "Content-Type": "application/json"
+    }
+
+    payload = {
+        "field_ids": ["name", "linkedin", "twitter", "facebook", "short_description", "listed_stock_symbol", "image_url", "identifier", "website_url", "location_identifiers"],
+        "query": [{
+            "type": "predicate",
+            "field_id": "identifier",
+            "operator_id": "contains",
+            "values": [company_name]
+        }]
+    }
+
+    print(f"payload: {payload}")
+
+
+    company_data = requests.post(f"{env("DJANGO_CRUNCHBASE_API_URL")}/data/searches/organizations", headers=headers, json=payload).json()
+
+    return Response(company_data)
--- server/config/settings.py
+++ server/config/settings.py
@@ -11,10 +11,19 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import environ
+
+env = environ.Env(
+    DEBUG=(bool, False)
+)
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+
+environ.Env.read_env(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
